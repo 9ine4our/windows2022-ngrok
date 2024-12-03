673f11b72e41dca3864fbce6b03f6825c4d4051c
9ine4our <148853314+9ine4our@users.noreply.github.com>
2024-12-03 16:40:51 +0700

    Update start-2022.bat

--- start-2022.bat
+++ start-2022.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2022 by @Davitt" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2022 by @TheBand1t" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user Administrator TheBand1t /add >nul
